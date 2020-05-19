08e2585a1e0d1210d218b9da651e95c1d6d96e97
suja <sujijothy@gmail.com>
2020-05-19 14:47:44 -0500

    basic coding events with database committed

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,12 +1,15 @@
 package org.launchcode.codingevents.controllers;
 
+import jdk.jfr.Description;
+import org.launchcode.codingevents.EventRepository;
+import org.launchcode.codingevents.models.Event;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,26 +19,89 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
+//@Controller
+//@RequestMapping("events")
+//public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+  //  private static List<Event> events = new ArrayList<Event>();
+    @Autowired
+    private EventRepository eventRepository;
 
     @GetMapping
-    public String displayAllEvents(Model model) {
-        model.addAttribute("title", "All Events");
-        model.addAttribute("events", events);
+    public String displayform(Model model){
+    model.addAttribute("title","All Events");
+    model.addAttribute("events",eventRepository.findAll());
         return "events/index";
     }
-
+    @GetMapping("index")
+    public String displayAllEvents(Model model){
+        model.addAttribute("title","All Events");
+        model.addAttribute("events",eventRepository.findAll());
+        return "events/index";
+    }
     @GetMapping("create")
-    public String displayCreateEventForm(Model model) {
-        model.addAttribute("title", "Create Event");
+    public String createform(Model model){
+        model.addAttribute("title","Create Events");
+        model.addAttribute(new Event());
         return "events/create";
     }
 
+
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(eventName);
+    public String Reqcreateform(@ModelAttribute @Valid Event newEvent, Errors errors,Model model) {
+        if(errors.hasErrors()) {
+            model.addAttribute("title", "Create Event");
+            return "events/create";
+        }
+        newEvent.setName(newEvent.getName());
+        newEvent.setDescription(newEvent.getDescription());
+        eventRepository.save(newEvent);
         return "redirect:";
+    }
+    @GetMapping("delete")
+    public String deleteform(Model model){
+        model.addAttribute("title","delete Events");
+        model.addAttribute("events",eventRepository.findAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String Reqdeleteform(@RequestParam(required = false) int[] eventIds){
+        if(eventIds != null ){
+            for (int eventId:eventIds){
+                  eventRepository.deleteById(eventId);
+        }
+    }
+
+    return "redirect:";
     }
 
 }
+
+
+//@Controller
+//@RequestMapping("events")
+//public class EventController {
+//
+//    private static List<Event> events = new ArrayList<>();
+//
+//    @GetMapping
+//    public String displayAllEvents(Model model) {
+//        model.addAttribute("title", "All Events");
+//        model.addAttribute("events", events);
+//        return "events/index";
+//    }
+//
+//    @GetMapping("create")
+//    public String displayCreateEventForm(Model model) {
+//        model.addAttribute("title", "Create Event");
+//        return "events/create";
+//    }
+//
+//    @PostMapping("create")
+//    public String processCreateEventForm(@RequestParam String eventName) {
+//        events.add(new Event(eventName));
+//        return "redirect:";
+//    }
+//
+//}
---
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -0,0 +1,65 @@
+package org.launchcode.codingevents.models;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Event {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String description;
+
+    public Event() {
+    }
+
+    public Event(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return (name + description);
+    }
+}
